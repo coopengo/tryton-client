368b391626f8e232ae7918a9b46795a9ce8625bc
romain seon <romain.seon@coopengo.com>
2014-04-01 17:24:26 +0200

    Use usable format for log datetime

--- tryton/config.py
+++ tryton/config.py
@@ -111,8 +111,8 @@ class ConfigManager(object):
         self.load()
 
         self.options['dev'] = opt.dev
-        format = '[%(asctime)s] %(levelname)s:%(name)s:%(message)s'
-        datefmt = '%a %b %d %H:%M:%S %Y'
+        format = '%(asctime)s %(levelname)s:%(name)s:%(message)s'
+        datefmt = '%Y-%m-%d %H:%M:%S'
         logging.basicConfig(format=format, datefmt=datefmt)
         loglevels = {
             'DEBUG': logging.DEBUG,

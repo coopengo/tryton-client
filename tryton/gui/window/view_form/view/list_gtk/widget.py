7a2ee3a1bc7be22df08643cf4f88347f40a3c6d3
Jean CAVALLO <jean.cavallo@coopengo.com>
2015-01-02 18:32:45 +0100

    Use both the record model and id as key for view_tree cache [CUSTOM]

--- tryton/gui/window/view_form/view/list_gtk/widget.py
+++ tryton/gui/window/view_form/view/list_gtk/widget.py
@@ -102,7 +102,8 @@ class CellCache(list):
                 self.cell_caches = {}
             record = store.get_value(iter_, 0)
             counter = self.view.treeview.display_counter
-            if (self.display_counters.get(record.id) != counter):
+            if (self.display_counters.get((record.model_name, record.id)) !=
+                    counter):
                 if getattr(cell, 'decorated', None):
                     func(self, column, cell, store, iter_, user_data)
                 else:
@@ -110,10 +111,11 @@ class CellCache(list):
                     cache.decorate(cell)
                     func(self, column, cell, store, iter_, user_data)
                     cache.undecorate(cell)
-                    self.cell_caches[record.id] = cache
-                    self.display_counters[record.id] = counter
+                    self.cell_caches[(record.model_name, record.id)] = cache
+                    self.display_counters[(record.model_name, record.id)] = \
+                        counter
             else:
-                self.cell_caches[record.id].apply(cell)
+                self.cell_caches[(record.model_name, record.id)].apply(cell)
         return wrapper
 
 

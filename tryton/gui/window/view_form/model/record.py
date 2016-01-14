4445eae2e9c1272f549d83d77a73ee52ccac0be7
Jean Cavallo <jean.cavallo@hotmail.fr>
2016-01-14 18:13:41 +0100

    Increase base id on records to lower risk of buggy conflicts with existing ids [CUSTOM]
    review http://rietveld.coopengo.com/17710001

--- tryton/gui/window/view_form/model/record.py
+++ tryton/gui/window/view_form/model/record.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 class Record:
 
-    id = -1
+    id = -100000000
 
     def __init__(self, model_name, obj_id, group=None):
         super(Record, self).__init__()

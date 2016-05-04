40691d81792c837be33363872ec45b7704cf18a6
Adrien BEN-DUC <adrien.benduc@gmail.com>
2016-05-04 18:05:23 +0200

    tryton: Avoid client crash on wrong date entry fix #3977 rietveld: http://rietveld.coopengo.com/24540001

--- tryton/common/datetime_.py
+++ tryton/common/datetime_.py
@@ -26,6 +26,7 @@ def date_parse(text, format_='%x'):
     dayfirst = formatted_date.index('16') == 0
     monthfirst = formatted_date.index('7') <= 1
     yearfirst = not dayfirst and not monthfirst
+    text = re.sub('/+', '/', text)
     if len(text) == 6 and re.search('[0-9]{6}', text):
         text = '%s/%s/%s' % (text[:2], text[2:4], text[4:6])
     elif len(text) == 8 and re.search('[0-9]{8}', text):
@@ -33,7 +34,14 @@ def date_parse(text, format_='%x'):
             text = '%s/%s/%s' % (text[:4], text[4:6], text[6:8])
         else:
             text = '%s/%s/%s' % (text[:2], text[2:4], text[4:8])
-    return parse(text, dayfirst=dayfirst, yearfirst=yearfirst, ignoretz=True)
+    elif text.endswith('/'):
+        text = text.strip('/')
+    # Try catch below avoid client crash when the parse method fails
+    try:
+        return parse(text, dayfirst=dayfirst, yearfirst=yearfirst,
+            ignoretz=True)
+    except:
+        return datetime.datetime.now()
 
 
 class Date(gtk.Entry):

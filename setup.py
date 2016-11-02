dc9195efe3754c3bc64a2ad64eaf3f05c21fe0b8
Jean Cavallo <jean.cavallo@coopengo.com>
2016-11-02 09:41:41 +0100

    tryton : Update version to 1.10

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ def get_version():
 PACKAGE=None
 WEBSITE='http://www.coopengo.com/'
 LICENSE=None
-version = '1.8.0'
+version = '1.10.0'
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)

185d80c467bf8b9e87da6a78bf9427220ca8378d
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2017-01-23 11:15:40 +0100

    Win build tool [CUSTOM]

---
+++ win-make.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+version() {
+  local t
+  t=$(git describe --tags --exact-match 2> /dev/null | grep "^coog-" | head -1)
+  if [ ! -z "$t" ]
+  then
+    echo "${t//coog-/}"
+  else
+    local b; b=$(git rev-parse --abbrev-ref HEAD)
+    local c; c=$(git rev-parse --short HEAD)
+    echo "$b-$c" | sed -e "s/coog-//g"
+  fi
+}
+
+deps() {
+  pacman -S \
+    mingw-w64-i686-librsvg \
+    mingw-w64-i686-nsis \
+    mingw-w64-i686-python2 \
+    mingw-w64-i686-python2-setuptools \
+    mingw-w64-i686-python2-pip \
+    mingw-w64-i686-python2-pygtk \
+    mingw-w64-i686-gtk-engine-murrine \
+    mingw-w64-i686-python2-cx_Freeze
+
+  pip install \
+    python-dateutil \
+    chardet \
+    pyflakes
+}
+
+clean() {
+  rm -rf build dist coog-*
+}
+
+patch() {
+  git apply win-patch.diff
+}
+
+unpatch() {
+  git checkout HEAD -- tryton
+}
+
+build() {
+  clean
+  patch
+  local v; v=$(version)
+  python setup-freeze.py install_exe -d dist
+  cp -rv ./share/themes/Coog/gtk-2.0/ ./dist/share/themes/MS-Windows/
+  makensis -DVERSION="$v" setup.nsi
+  makensis -DVERSION="$v" setup-single.nsi
+  unpatch
+}
+
+main() {
+  export PATH="$PATH:/mingw32/bin"
+  [ -z "$1" ] && echo missing command && return 1
+  "$1" "$@"
+}
+
+main "$@"

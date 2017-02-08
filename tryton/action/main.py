139697b46d0d9a721d5d9bf45ee61a66e028aef4
Adrien BEN-DUC <adrien.benduc@coopengo.com>
2017-02-08 09:07:23 +0100

    Add comment

--- tryton/action/main.py
+++ tryton/action/main.py
@@ -43,6 +43,8 @@ class Action(object):
             print_p = True
         dtemp = tempfile.mkdtemp(prefix='tryton_')
 
+        # We now support list. Mainly for the multi-attachment feature:
+        # see #5658 for further informations.
         if type(names) is not list:
             names = [names]
         if type(datas) is not list:

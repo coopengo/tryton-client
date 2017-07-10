55700a14f5de6c433cbb69cd38a8dada78fec91c
Pierre Jackman <pierre.jackman@coopengo.com>
2017-07-10 10:45:00 +0200

    Add support for toggling view [CUSTOM]
    PR: (#13)

--- tryton/gui/window/view_form/screen/screen.py
+++ tryton/gui/window/view_form/screen/screen.py
@@ -1027,6 +1027,7 @@ class Screen:
                         or view.view_type == 'tree'
                         or view.widget.get_parent()):
                     view.display()
+
             self.current_view.widget.set_sensitive(
                 bool(self.group
                     or (self.current_view.view_type != 'form')
@@ -1327,11 +1328,20 @@ class Screen:
                 ids, context=context)
         except RPCException:
             action = None
+
+        # PJA: handle different returns values from button
+        if isinstance(action, list):
+            action_id, action = action
+        elif isinstance(action, int):
+            action_id, action = action, None
+        else:
+            action_id, action = None, action
+
         self.reload(ids, written=True)
         if isinstance(action, str):
             self.client_action(action)
-        elif action:
-            Action.execute(action, {
+        if action_id:
+            Action.execute(action_id, {
                     'model': self.model_name,
                     'id': self.current_record.id,
                     'ids': ids,

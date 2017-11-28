0106a477655b9182348dc8f16cbeed3281ef2d58
adrien.benduc <adrien.benduc@coopengo.com>
2017-11-28 17:54:23 +0100

    tryton: Fix client version

--- tryton/__init__.py
+++ tryton/__init__.py
@@ -1,4 +1,4 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 __version__ = "4.4.4"
-__version_coog__ = "1.13"
+__version_coog__ = "1.14"

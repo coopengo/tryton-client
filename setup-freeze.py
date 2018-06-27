ccca6475490ffea68ad1ea5e6f36eecc95211bc4
Adrien BEN DUC <adrien.benduc@coopengo.com>
2018-06-27 12:25:21 +0000

    Compile working [CUSTOM]

--- setup-freeze.py
+++ setup-freeze.py
@@ -28,6 +28,8 @@ include_files = [
         os.path.join('share', 'glib-2.0', 'schemas')),
     (os.path.join(sys.prefix, 'lib', 'gtk-3.0'),
         os.path.join('lib', 'gtk-3.0')),
+    (os.path.join(sys.prefix, 'lib', 'girepository-1.0'),
+        os.path.join('lib', 'girepository-1.0')),
     (os.path.join(sys.prefix, 'lib', 'gdk-pixbuf-2.0'),
         os.path.join('lib', 'gdk-pixbuf-2.0')),
     (os.path.join(sys.prefix, 'lib', 'evince'),
@@ -41,6 +43,10 @@ include_files = [
     (os.path.join(sys.platform, 'gtk-3.0', 'gdk-pixbuf.loaders'),
         os.path.join('etc', 'gtk-3.0', 'gdk-pixbuf.loaders')),
     ]
+
+BIN_DIR = (os.path.join(sys.prefix, 'bin'))
+for dll in [x for x in os.listdir(BIN_DIR) if x.endswith('.dll')]:
+    include_files.append(os.path.join(sys.prefix, 'bin', dll))
 
 required_gi_namespaces = [
     'Atk-1.0',
@@ -130,12 +136,14 @@ setup(name='tryton',
     version=version,
     options={
         'build_exe': {
-            'no_compress': True,
+
             'include_files': include_files,
             'excludes': ['tkinter'],
             'silent': True,
             'packages': ['gi'],
+            'includes': ['gi', "gi.overrides.Gtk"],
             'include_msvcr': True,
+            'path': sys.path.append("girepository-1.0"),
             },
         'bdist_mac': {
             'iconfile': os.path.join(

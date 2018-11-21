74880dc131dabb2f778c67b08aa0873b49e55b6f
Nidhal Khalgui <nidhal.khalgui@coopengo.com>
2018-11-21 17:56:29 +0100

    Fix Required Fields Border Colors [CUSTOM]
    PR: (#55)
    Fix #10137

--- tryton/client.py
+++ tryton/client.py
@@ -43,7 +43,9 @@ def main():
     screen = Gdk.Screen.get_default()
     style_context = Gtk.StyleContext()
     provider = Gtk.CssProvider()
-    provider.load_from_data(CSS)
+    # the line below injects CSS with higher priority than the custom theme
+    # -> it overrides Coog theme behavior
+    # provider.load_from_data(CSS)
     style_context.add_provider_for_screen(
         screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
     theme_path = os.path.join(get_config_dir(), 'theme.css')

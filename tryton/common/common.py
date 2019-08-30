749b1012b7f8d05b9b8b403c6f59b1187fac99f8
Jean Cavallo <jean.cavallo@hotmail.fr>
2019-08-30 11:33:55 +0200

    Fix client_defined_date [CUSTOM]
    PR: (#83)

--- tryton/common/common.py
+++ tryton/common/common.py
@@ -1203,6 +1203,8 @@ def RPCContextReload(callback=None):
             rpc.CONTEXT.update(context())
         except RPCException:
             pass
+        if rpc._CLIENT_DATE:
+            rpc.CONTEXT['client_defined_date'] = rpc._CLIENT_DATE
         if callback:
             callback()
     context = RPCExecute(
@@ -1211,6 +1213,8 @@ def RPCContextReload(callback=None):
     if not callback:
         rpc.context_reset()
         rpc.CONTEXT.update(context)
+        if rpc._CLIENT_DATE:
+            rpc.CONTEXT['client_defined_date'] = rpc._CLIENT_DATE
 
 
 class Tooltips(object):

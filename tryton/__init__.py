bbb8af3e0b8d6b8d501ae175aab23ffd5bd5e729
Jérémy Mousset <jeremy.mousset@coopengo.com>
2019-09-19 15:39:25 +0200

    tryton: Use locale.localize() and new format to format Decimal
    picked from : https://bugs.tryton.org/issue8574

--- tryton/__init__.py
+++ tryton/__init__.py
@@ -2,6 +2,7 @@
 # this repository contains the full copyright notices and license terms.
 __version_coog__ = "2.5"
 __version__ = "5.2.3"
+import locale
 import gi
 
 gi.require_version('Gtk', '3.0')
@@ -18,3 +19,25 @@ try:
     import goocalendar
 except ImportError:
     pass
+
+if not hasattr(locale, 'localize'):
+    def localize(formatted, grouping=False, monetary=False):
+        float(formatted)
+        if '.' in formatted:
+            seps = 0
+            parts = formatted.split('.')
+            if grouping:
+                parts[0], seps = locale._group(parts[0], monetary=monetary)
+            decimal_point = locale.localeconv()[
+                monetary and 'mon_decimal_point' or 'decimal_point']
+            formatted = decimal_point.join(parts)
+            if seps:
+                formatted = locale._strip_padding(formatted, seps)
+        else:
+            seps = 0
+            if grouping:
+                formatted, seps = locale._group(formatted, monetary=monetary)
+            if seps:
+                formatted = locale._strip_padding(formatted, seps)
+        return formatted
+    setattr(locale, 'localize', localize)

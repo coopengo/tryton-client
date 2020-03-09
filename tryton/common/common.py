74fd5ba97b9a8090b37555cf8de0f1deb41687bb
ClementArnouxCoopengo <59828917+ClementArnouxCoopengo@users.noreply.github.com>
2020-03-09 09:58:50 +0100

    Fix warning crash [CUSTOM]
    PR: (#97)

--- tryton/common/common.py
+++ tryton/common/common.py
@@ -721,8 +721,8 @@ class UserWarningDialog(WarningDialog):
 
     def process_response(self, response):
         if response == Gtk.ResponseType.YES:
-            if self.always.get_active():
-                return 'always'
+            # if self.always.get_active():
+            #     return 'always'
             return 'ok'
         return 'cancel'
 

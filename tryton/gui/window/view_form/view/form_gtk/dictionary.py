628c73538278b1488a0b355011ef5fcb21d0a782
Nidhal <nidhal.khalgui@coopengo.com>
2020-03-26 15:28:34 +0100

    Revert "dict: Remove remove button if no_command is True (#99)"
    This reverts commit 6b8dfc9fe9aed1520d79f4dd1ba6d760855e87d9.

--- tryton/gui/window/view_form/view/form_gtk/dictionary.py
+++ tryton/gui/window/view_form/view/form_gtk/dictionary.py
@@ -463,7 +463,8 @@ class DictWidget(Widget):
 
     def _sig_remove(self, button, key, modified=True):
         del self.fields[key]
-        del self.buttons[key]
+        if not self.attrs.get('no_command', 0.0) and self.buttons.get(key):
+            del self.buttons[key]
         for widget in self.rows[key]:
             self.grid.remove(widget)
             widget.destroy()
@@ -525,16 +526,15 @@ class DictWidget(Widget):
         self.grid.attach_next_to(
             hbox, label, Gtk.PositionType.RIGHT, 1, 1)
         hbox.show_all()
-        if not self.attrs.get('no_command', 0.0):
-            remove_but = self._new_remove_btn()
-            self.tooltips.set_tip(remove_but, _('Remove "%s"') %
-                key_schema['string'])
-            self.grid.attach_next_to(
-                remove_but, hbox, Gtk.PositionType.RIGHT, 1, 1)
-            remove_but.connect('clicked', self._sig_remove, key)
-            remove_but.show_all()
-            self.rows[key] = [label, hbox, remove_but]
-            self.buttons[key] = remove_but
+        remove_but = self._new_remove_btn()
+        self.tooltips.set_tip(remove_but, _('Remove "%s"') %
+            key_schema['string'])
+        self.grid.attach_next_to(
+            remove_but, hbox, Gtk.PositionType.RIGHT, 1, 1)
+        remove_but.connect('clicked', self._sig_remove, key)
+        remove_but.show_all()
+        self.rows[key] = [label, hbox, remove_but]
+        self.buttons[key] = remove_but
 
     def display(self):
         super(DictWidget, self).display()

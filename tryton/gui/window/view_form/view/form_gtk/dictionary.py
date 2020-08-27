079f45ef63adef0c7c51a295fe0d0111408cc52e
Nidhal Khalgui <nidhal.khalgui@coopengo.com>
2020-08-27 17:32:33 +0200

    Fixes post merge [CUSTOM]
    PR: (#120)
    OTH #0000
    * fix multiselection in dictionnaries first display
    * variable declaration missing

--- tryton/gui/window/view_form/view/form_gtk/dictionary.py
+++ tryton/gui/window/view_form/view/form_gtk/dictionary.py
@@ -227,7 +227,7 @@ class DictMultiSelectionEntry(DictEntry):
         selection.handler_block_by_func(self._changed)
         try:
             selection.unselect_all()
-            for v in value:
+            for v in value or []:
                 if v in value2path:
                     selection.select_path(value2path[v])
         finally:
@@ -571,7 +571,6 @@ class DictWidget(Widget):
 
     def display(self):
         super(DictWidget, self).display()
-
         if not self.field:
             return
 

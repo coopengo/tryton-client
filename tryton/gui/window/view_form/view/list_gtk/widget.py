85916c6aff216238e79a73b03c2f60b86abe0746
Nicolas Évrard <nicoe@users.noreply.github.com>
2020-11-06 12:49:50 +0100

    Use editable path to update editable on display [CUSTOM]
    (#134)

--- tryton/gui/window/view_form/view/list_gtk/widget.py
+++ tryton/gui/window/view_form/view/list_gtk/widget.py
@@ -467,7 +467,7 @@ class Boolean(GenericText):
             self.view.treeview.set_cursor(path)
         return True
 
-    def set_editable(self, record):
+    def set_editable(self):
         pass
 
 

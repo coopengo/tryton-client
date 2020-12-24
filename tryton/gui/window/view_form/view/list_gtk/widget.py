e47132dfc1d32b20f7847154c755425279b9516e
Nicolas Évrard <nicoe@users.noreply.github.com>
2020-12-24 14:45:28 +0100

    Backport fixes related to synchronization between cell and editable content (#139) (#141)
    * Backport fixes related to synchronization between cell and editable content
    
    * Replace call to get_value by field.get
    
    * Fix some set_editable signature missed

--- tryton/gui/window/view_form/view/list_gtk/widget.py
+++ tryton/gui/window/view_form/view/list_gtk/widget.py
@@ -118,6 +118,16 @@ class CellCache(list):
 
 class Cell(object):
 
+    def _get_record_field_from_path(self, path, store=None):
+        if not store:
+            store = self.view.treeview.get_model()
+        record = store.get_value(store.get_iter(path), 0)
+        field = record.group.fields[self.attrs['name']]
+        return record, field
+
+    def set_editable(self):
+        pass
+
     def get_color(self, record):
         return record.expr_eval(self.view.attributes.get('colors', '"black"'))
 
@@ -186,6 +196,7 @@ class Affix(Cell):
 class GenericText(Cell):
     align = 0
     editable = None
+    editing = None
 
     def __init__(self, view, attrs, renderer=None):
         super(GenericText, self).__init__()
@@ -336,13 +347,18 @@ class GenericText(Cell):
         if callback:
             callback()
 
-    def set_editable(self, record):
+    def set_editable(self):
+        if not self.editable or not self.editing:
+            return
+        record, field = self.editing
         self.editable.set_text(self.get_textual_value(record))
 
     def editing_started(self, cell, editable, path):
         def remove(editable):
             self.editable = None
+            self.editing = None
         self.editable = editable
+        self.editing = self._get_record_field_from_path(path)
         editable.connect('remove-widget', remove)
         return False
 
@@ -409,7 +425,7 @@ class Boolean(GenericText):
             self.view.treeview.set_cursor(path)
         return True
 
-    def set_editable(self, record):
+    def set_editable(self):
         pass
 
 
@@ -910,9 +926,11 @@ class Selection(GenericText, SelectionMixin, PopdownMixin):
         if callback:
             callback()
 
-    def set_editable(self, record):
-        field = record[self.attrs['name']]
-        value = self.get_value(record, field)
+    def set_editable(self):
+        if not self.editable or not self.editing:
+            return
+        record, field = self.editing
+        value = field.get(record)
         self.update_selection(record, field)
         self.set_popdown_value(self.editable, value)
 

ba101fe1f3955e9d414df0c85286bef77833bd85
Jean Cavallo <jean.cavallo@hotmail.fr>
2021-01-20 10:12:31 +0100

    Store clicked id before the view is reloaded [CUSTOM]
    c8c7f34d was supposed to fix this, but it did not

--- tryton/gui/window/view_form/screen/screen.py
+++ tryton/gui/window/view_form/screen/screen.py
@@ -1328,6 +1328,7 @@ class Screen:
 
     def _button_class(self, button):
         ids = [r.id for r in self.selected_records]
+        current_id = self.current_record.id
         context = self.context
         context['_timestamp'] = {}
         for record in self.selected_records:
@@ -1352,7 +1353,7 @@ class Screen:
         if action_id:
             Action.execute(action_id, {
                     'model': self.model_name,
-                    'id': self.current_record.id,
+                    'id': current_id,
                     'ids': ids,
                     }, context=self.context, keyword=True)
 

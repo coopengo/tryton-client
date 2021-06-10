7b2dcd11bbfebcf82783776cbeec592bb7518b60
Nicolas Evrard <nicoe@openhex.org>
2021-06-10 12:48:38 +0200

    Add option to remove leading and trailing whitespaces from fields.Char

--- tryton/gui/window/view_form/model/field.py
+++ tryton/gui/window/view_form/model/field.py
@@ -218,6 +218,10 @@ class Field(object):
 class CharField(Field):
     _default = ''
 
+    def set(self, record, value):
+        super().set(
+            record, value.strip() if self.attrs.get('strip') else value)
+
     def get(self, record):
         return super(CharField, self).get(record) or self._default
 

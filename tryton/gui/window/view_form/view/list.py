9dfb347b90ac5d7ab351a167ded6b75cbd41f3d6
Audrey <audrey.gonthier@coopengo.com>
2021-08-20 09:09:14 +0200

    fix v6

--- tryton/gui/window/view_form/view/list.py
+++ tryton/gui/window/view_form/view/list.py
@@ -1260,7 +1260,8 @@ class ViewTree(View):
             records.append(model.get_value(iter_, 0))
         records = []
         sel = self.treeview.get_selection()
-        sel.selected_foreach(_func_sel_get, records)
+        if sel:
+            sel.selected_foreach(_func_sel_get, records)
         return records
 
     def get_selected_paths(self):

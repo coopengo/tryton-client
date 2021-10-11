c297b4c1d67833e9a266c7f07f21bd48f05d43d8
mpaulmier <matthias.paulmier@coopengo.com>
2021-10-11 15:24:03 +0200

    [2.12] fix icons and app name

--- tryton/gui/window/about.py
+++ tryton/gui/window/about.py
@@ -10,7 +10,8 @@ from tryton.common import get_toplevel_window
 from tryton import __version_coog__
 
 COPYRIGHT = '''\
-Copyright (C) 2004-2020 Tryton.
+Copyright (C) 2004-2021 Tryton.
+Copyright (C) 2012-2021 Coopengo.
 '''
 AUTHORS = [
         'Bertrand Chenal <bertrand.chenal@b2ck.com>',
@@ -32,6 +33,7 @@ class About(object):
     def __init__(self):
         parent = get_toplevel_window()
         self.win = Gtk.AboutDialog()
+        self.win.set_program_name('Coog')
         self.win.set_transient_for(parent)
         self.win.set_name(CONFIG['client.title'])
         # MAR : Fix #5107 : Replace tryton references with Coog
@@ -44,7 +46,7 @@ class About(object):
         self.win.set_authors(AUTHORS)
         self.win.set_translator_credits(_('translator-credits'))
         self.win.set_logo(GdkPixbuf.Pixbuf.new_from_file(
-                os.path.join(PIXMAPS_DIR, 'tryton.svg')))
+                os.path.join(PIXMAPS_DIR, 'coog_text.svg')))
         self.win.run()
         parent.present()
         self.win.destroy()

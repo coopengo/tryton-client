820ad8f21c0520cf7556f782217ee7686aa2da13
Nicolas Évrard <nicoe@users.noreply.github.com>
2022-04-14 15:11:09 +0200

    Add database timeout parameter on RPC calls (#201)
    Fix #22550
    
    * Add database timeout parameter on RPC calls

--- tryton/jsonrpc.py
+++ tryton/jsonrpc.py
@@ -19,6 +19,11 @@ from functools import reduce
 from urllib.parse import urljoin, quote, urlparse
 from urllib.request import getproxies
 
+try:
+    from http import HTTPStatus
+except ImportError:
+    from http import client as HTTPStatus
+
 __all__ = ["ResponseError", "Fault", "ProtocolError", "Transport",
     "ServerProxy", "ServerPool"]
 CONNECT_TIMEOUT = 5
@@ -353,6 +358,8 @@ class ServerProxy(xmlrpc.client.ServerProxy):
                     verbose=self.__verbose
                     )
         except xmlrpc.client.ProtocolError as e:
+            if e.errcode == HTTPStatus.UNAUTHORIZED:
+                raise ProtocolError(self.__host, e.errcode, e.errmsg, {})
             raise Fault(str(e.errcode), e.errmsg)
         except Exception:
             self.__transport.close()

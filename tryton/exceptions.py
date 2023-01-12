2e0c03b13619e83c6a81e9fb2ff0ae5e32ea5b76
Nicolas Évrard <nicoe@users.noreply.github.com>
2023-01-12 17:39:11 +0100

    [coog-22.41] Unauthorized error (#235)
    Fix #24586

--- tryton/exceptions.py
+++ tryton/exceptions.py
@@ -1,9 +1,9 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from .jsonrpc import Fault, ProtocolError
+from .jsonrpc import Fault
 
 TrytonServerError = Fault
-TrytonAuthenticationError = ProtocolError
+TrytonAuthenticationError = Fault
 
 
 class TrytonServerUnavailable(Exception):

738e3df25f1c2a0c2b7e551c415dd38d192007af
coopengo-glecomte <85690431+coopengo-glecomte@users.noreply.github.com>
2023-01-30 15:18:08 +0100

    Get portable version in .zip file + sign clients (#241)
    #24602
    #24591
    
    (cherry picked from commit 7bb42cb187aedcc2de8e2a102f6cb8a8fb7e05c5)

---
+++ sign-client.bat
@@ -0,0 +1,11 @@
+@echo off
+set password=%1
+set cert_path="C:\Users\Administrator\Desktop\certificat\coopengo-software-cert.pfx"
+set "current_directory=%cd%"
+
+dir %current_directory%
+
+FOR /F "tokens=*" %%f in ('dir /b /a-d coog-*') do (
+  echo %%f
+  start signtool sign /f "%cert_path%" /p "%password%" /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "%%f"
+)

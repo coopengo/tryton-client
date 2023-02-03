fa43e79605c850cc29db4399a87a23a06fcf0718
🤖 GitLab CI/CD 🤖 <gitlab@coopengo.com>
2023-02-03 16:11:52 +0000

    Merge .gitlab-ci.yml, sign-client.bat and build directory from "e6836912"

--- sign-client.bat
+++ sign-client.bat
@@ -6,11 +6,11 @@ set folder=C:\msys32\home\Administrator\tryton\dist
 
 if exist %folder% (
   echo Signing coog.exe ...
-  "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /f %cert_path% /p %password% /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "%folder%\coog.exe"
+  "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%folder%\coog.exe"
 ) else (
   FOR /F "tokens=*" %%f in ('dir /b /a-d coog-*.exe') do (
     echo Signing %%f ...
-    "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /f %cert_path% /p %password% /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "%%f"
+    "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%%f"
   )
 )
 

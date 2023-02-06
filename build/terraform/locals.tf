36efcabd521985a2c3b47f225e0dc6b97e9a8c29
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-02-06 16:03:02 +0100

    remove build directory

--- build/terraform/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  common_tags = {
-    Name        = lower(var.name)
-    Owner       = lower(var.owner)
-    Managed_by  = lower("terraform")
-    Environment = lower(var.environment)
-    Project     = lower("tryton")
-  }
-}

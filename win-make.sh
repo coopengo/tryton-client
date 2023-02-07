4337e42743bfa7b46fa87147521eb2b6b9ca2630
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-02-07 11:14:28 +0100

    Gdrive: migrate client upload to bot@coopengo.com
    (cherry picked from commit 9d8c828bc22e796daeed8179f53fcb5cccf20de2)

--- win-make.sh
+++ win-make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GDRIVE_FOLDER_ID=1M-aW5jKN5cDSJp7zMdaDEPV1r7t6dYry
+GDRIVE_FOLDER_ID=1zSU-YN360SwLUuknwWW63BHVgJa6kU8_
 
 version() {
     local t
@@ -37,7 +37,7 @@ deps() {
         chardet \
         pyflakes
 
-    echo "gdrive should be installed from https://github.com/prasmussen/gdrive#downloads"
+    echo "gdrive should be installed from https://github.com/glotlabs/gdrive#downloads"
     echo "gdrive should be placed in a PATH folder"
 }
 
@@ -56,7 +56,7 @@ build() {
 upload() {
     for f in ./coog-*
     do
-        gdrive upload -p "$GDRIVE_FOLDER_ID" "$f"
+        gdrive upload --parent "$GDRIVE_FOLDER_ID" "$f"
     done
 }
 

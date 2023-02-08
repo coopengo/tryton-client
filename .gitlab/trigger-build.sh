3450eb592de4c9287d4c14011ff2798646dba539
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-02-08 17:25:18 +0100

    test

--- .gitlab/trigger-build.sh
+++ .gitlab/trigger-build.sh
@@ -9,5 +9,5 @@ cd tryton
 /usr/bin/git reset --hard origin/master
 /usr/bin/git clean -fd
 /usr/bin/git checkout "${CI_COMMIT_REF_NAME:?}"
-source .gitlab/env/windows.env
-./win-make.sh build "${COOPENGO_SOFTWARE_CERT_PASSWORD:?}"
+source $(pwd)/.gitlab/env/windows.env
+$(pwd)/win-make.sh build "${COOPENGO_SOFTWARE_CERT_PASSWORD:?}"

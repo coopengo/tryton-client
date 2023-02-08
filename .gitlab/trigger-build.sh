9b22bd2c2f78093b6940cdc23c7d75fe1ed4f89f
🤖 GitLab CI/CD 🤖 <gitlab@coopengo.com>
2023-02-08 14:41:56 +0000

    Merge .gitlab-ci.yml, sign-client.bat and build directory from "ebf48faa"

---
+++ .gitlab/trigger-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+PATH="${PATH}:/c/msys32/mingw32/bin"
+
+cd tryton
+/usr/bin/git fetch --tags --all -p -f
+/usr/bin/git reset --hard origin/master
+/usr/bin/git clean -fd
+/usr/bin/git checkout "${CI_COMMIT_REF_NAME:?}"
+source .gitlab/env/windows.env
+./win-make.sh build "${COOPENGO_SOFTWARE_CERT_PASSWORD:?}"

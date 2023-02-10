f781f9d7f956094dcd9ded4ec8dda7c2ec6060b6
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-02-10 11:47:26 +0100

    test

--- sign-client.bat
+++ sign-client.bat
@@ -9,11 +9,11 @@ set folder=C:\msys32\home\Administrator\tryton\dist
 
 if exist %folder% (
   echo Signing coog.exe ...
-  "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%folder%\coog.exe"
+  "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /v /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%folder%\coog.exe"
 ) else (
   FOR /F "tokens=*" %%f in ('dir /b /a-d coog-*.exe') do (
     echo Signing %%f ...
-    "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%%f"
+    "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /v /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%%f"
   )
 )
 

1511ba988dcf22a0f02a7e0303f6f6a233aa0c75
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-02-10 15:28:35 +0100

    test

--- .gitlab/trigger-build.sh
+++ .gitlab/trigger-build.sh
@@ -4,11 +4,10 @@ set -x
 
 PATH="${PATH}:/c/msys32/mingw32/bin"
 
-cd tryton || exit 1
-source "$(pwd)/.gitlab/env/windows.env"
+source .gitlab/env/windows.env
 git fetch --tags --all -p -f
 git reset --hard origin/master
 git clean -fd
 git checkout "${CI_COMMIT_REF_NAME:?}"
 git pull --all --tags -f -p
-"$(pwd)/win-make.sh" build "${COOPENGO_SOFTWARE_CERT_PASSWORD:?}"
+win-make.sh build "${COOPENGO_SOFTWARE_CERT_PASSWORD:?}"

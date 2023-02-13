3519efc77d10d8e5ce12cb906c24ce0c2e439254
🤖 GitLab CI/CD 🤖 <gitlab@coopengo.com>
2023-02-13 11:52:56 +0000

    Merge .gitlab-ci.yml, sign-client.bat and build directory from "50e6077e"

--- sign-client.bat
+++ sign-client.bat
@@ -4,6 +4,8 @@ set cert_path=C:\Users\Administrator\Desktop\certificat\coopengo-software-cert.p
 set current_directory=%cd%
 set folder=C:\msys32\home\Administrator\tryton\dist
 
+cd "C:\msys32\home\Administrator\tryton"
+
 if exist %folder% (
   echo Signing coog.exe ...
   "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /td SHA256 /fdws /f %cert_path% /p %password% /tr http://timestamp.digicert.com "%folder%\coog.exe"

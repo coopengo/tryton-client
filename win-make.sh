55719a8d17b33d38e0e3c9265df6e8516487042a
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-04-04 11:04:17 +0200

    Setup for GitHub release

--- win-make.sh
+++ win-make.sh
@@ -64,9 +64,14 @@ build() {
 }
 
 upload() {
+    local v; v=$(version)
+
     for f in ./coog-*
     do
-        gdrive files upload --parent "$GDRIVE_FOLDER_ID" "$f"
+        CREATE_RELEASE=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/coopengo/tryton/releases '{"tag_name":"${v}","target_commitish":"${CI_COMMIT_REF_NAME}","name":"${v}","body":"Coog client for ${v}"}')
+        UPLOAD_URL=$(echo ${CREATE_RELEASE} | jq -r '.upload_url' | sed 's/{?name,label}//')
+        curl -X POST -H "Content-Type: application/octet-stream" --data-binary "@${f}" -H "Authorization: Bearer ${GITHUB_TOKEN}" "${UPLOAD_URL}?name=${f}"
+        # gdrive files upload --parent "$GDRIVE_FOLDER_ID" "$f"
     done
 }
 

b78aeb9ec1672ad6bfcd7d3bbf39af0931c32511
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-04-04 15:48:53 +0200

    fix data in CREATE_RELEASE

--- win-make.sh
+++ win-make.sh
@@ -72,7 +72,7 @@ build() {
 upload() {
     local v; v=$(version)
 
-    CREATE_RELEASE=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/coopengo/tryton/releases -d "{\"tag_name\":\"coog-$v\",\"target_commitish\":\"$CI_COMMIT_REF_NAME\",\"name\":\"coog-$v\",\"body\":\"Coog client for coog-$v\"}")
+    CREATE_RELEASE=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/coopengo/tryton/releases -d "{\"tag_name\":\"coog-$v\",\"name\":\"coog-$v\",\"body\":\"Coog client for coog-$v\"}")
     UPLOAD_URL=$(echo "${CREATE_RELEASE}" | jq -r '.upload_url' | sed 's/{?name,label}//')
 
     for f in ./coog-*

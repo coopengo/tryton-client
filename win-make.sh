53a2cb396f56aecfd41a7a778dce623199330090
Grégory Lecomte <gregory.lecomte@coopengo.com>
2023-04-04 11:59:29 +0200

    fix CREATE_RELEASE query

--- win-make.sh
+++ win-make.sh
@@ -72,8 +72,8 @@ build() {
 upload() {
     local v; v=$(version)
 
-    CREATE_RELEASE=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/coopengo/tryton/releases '{"tag_name":"${v}","target_commitish":"${CI_COMMIT_REF_NAME}","name":"${v}","body":"Coog client for ${v}"}')
-    UPLOAD_URL=$(echo ${CREATE_RELEASE} | jq -r '.upload_url' | sed 's/{?name,label}//')
+    CREATE_RELEASE=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/coopengo/tryton/releases '{"tag_name":"coog-$v","target_commitish":"$CI_COMMIT_REF_NAME","name":"coog-$v","body":"Coog client for coog-$v"}')
+    UPLOAD_URL=$(echo "${CREATE_RELEASE}" | jq -r '.upload_url' | sed 's/{?name,label}//')
 
     for f in ./coog-*
     do

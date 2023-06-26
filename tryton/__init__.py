59262910014b925d90df947b07f4b3870d77d7fa
🤖 GitLab CI/CD 🤖 <gitlab@coopengo.com>
2023-06-26 10:05:42 +0000

    Packaging coog-23.26.2324

--- tryton/__init__.py
+++ tryton/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-__version_coog__ = "23.25"
+__version_coog__ = "23.26"
 __version__ = "6.4.2"
 import locale
 
